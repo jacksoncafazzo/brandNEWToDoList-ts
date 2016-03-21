989d8f45d5e6afe4d27f92abe00d6fa788e9b72f
Chris Houghton and Jackson Cafazzo <student@epicodus.com>
2016-03-21 16:41:16 -0700

    display list of tasks and show them in the console when clicked

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from 'angular2/core';
 
 @Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  template: `
+  <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
+    {{ currentTask.description }}
+  </h3>
+  `
+})
+export class TaskListComponent {
+  public taskList: Task[];
+  taskClicked(clickedTask: Task): void {
+    console.log(clickedTask);
+  }
+}
+
+@Component({
   selector: 'my-app',
   template:`
-    <h1>Skeletor Angular2 App!</h1>
+  <div class="container">
+    <h1>To-Do List</h1>
+    <task-list [taskList]="tasks"></task-list>
+    </div>
+    <div>
     `
-})
+  })
 
-export class AppComponent {}
+
+export class AppComponent {
+  public tasks: Task[];
+  constructor(){
+    this.tasks = [
+      new Task("Create To Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+    ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log(clickedTask);
+  }
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {
+
+  }
+}
